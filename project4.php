493279fbddc7fb7403972a7c7b0758b5fb658abe
Kevin Flaherty <kevinjflaherty6@gmail.com>
2015-04-13 21:41:22 -0400

    Project4.php page commit
    Project page for friend burial project

--- project4.php
+++ project4.php
@@ -1,6 +1,6 @@
 <?php
 include "sitecake/sitecake/server/sitecake_entry.php";
-?>
+ ?>
 <!DOCTYPE html>
 
 <html lang="en">
@@ -8,14 +8,15 @@ include "sitecake/sitecake/server/sitecake_entry.php";
 	<head>
 		<link rel="icon"
 		type="image/png"
-		href="photos/chrislogo.png">
+		href="photos/Apoquague Friends Burial Grounds.JPG">
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<!-- Latest compiled and minified CSS -->
 
 		<!-- Optional theme -->
-
+		<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+		<link href="bootstrap-3.3.4-dist/css/bootstrap-responsive.css" rel="stylesheet" />
 		<link rel="stylesheet" type="text/css" href="bootstrap-3.3.4-dist/css/bootstrap.min.css">
 		<!-- Latest compiled and minified JavaScript -->
 
@@ -32,7 +33,7 @@ include "sitecake/sitecake/server/sitecake_entry.php";
 		<style type="text/css">
 			body {
 
-				background: url(photos/Projects.JPG) no-repeat center center fixed;
+				background: url("photos/Apoquague Friends Burial Grounds.JPG") no-repeat center center fixed;
 				-webkit-background-size: cover;
 				-moz-background-size: cover;
 				-o-background-size: cover;
@@ -51,6 +52,7 @@ include "sitecake/sitecake/server/sitecake_entry.php";
 	<header>
 
 		<nav class="navbar navbar-default navbar-fixed-top">
+
 			<div class="container-fluid">
 				<!-- Brand and toggle get grouped for better mobile display -->
 				<div class="navbar-header">
@@ -62,29 +64,29 @@ include "sitecake/sitecake/server/sitecake_entry.php";
 					</button>
 				</div>
 				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-
 					<ul class="nav navbar-nav">
-						<li>
-							<a href="index.php">Home</a>
+						<li class="active">
+							<a href="index.php">Home<span class="sr-only">(current)</span></a>
 						</li>
 						<li>
 							<a href="about.php">About Us</a>
 						</li>
-						<li class="active">
-							<a href="projects.php">Projects<span class="sr-only">(current)</span></a>
+						<li>
+							<a href="projects.php">Projects</a>
 						</li>
 						<li>
-							<a href="News.php">News</a>
+							<a href="news.php">News</a>
 						</li>
 						<li>
-							<a href="#">Contact</a>
+							<a href="contacts.php">Contact</a>
 						</li>
 						<li>
-							<a href="#">Donate</a>
+							<a href="donate.php">Donate</a>
 						</li>
 					</ul>
 				</div>
-				<!-- Collect the nav links, forms, and other content for toggling -->
+			</div>
+			<!-- Collect the nav links, forms, and other content for toggling -->
 
 			</div><!-- /.navbar-collapse -->
 		</nav>
@@ -97,24 +99,37 @@ include "sitecake/sitecake/server/sitecake_entry.php";
 		<!-- /.container-fluid -->
 
 		<div class="container" >
-			<div class="jumbotron myBackground sc-content-projects">
-				<h1>Projects</h1>
+			<div class="jumbotron myBackground sc-content-project4">
+				<h1 class="whitney">Overview</h1>
+
 				<p>
-				<li>
-					<h3><a href="project1.php">Patent Model Museum, 2015</a></h3>
-				</li>
-				<li>
-					<h3><a href="#">Artifacts at the Beekman Mill House, 2014</a></h3>
-				</li>
-				<li>
-					<h3><a href="project2.php">Beekman Mill House, 2014</a></h3>
-				</li>
-				<li>
-					<h3><a href="project4.php">Apouquague Friends Burial Ground, 2012</a></h3>
-				</li>
-</p>
+					At the foothills of Noble Hill Drive and Pleasant Ridge road, in the Hamlet of Poughquag, exists a two hundred year old Quaker cemetery that dates back to the founding of the Town of Beekman.
+				</p>
+				
+				<p>
+					This cemetery remained neglected for decades, and was the catalytic factor that helped create YFR. “I’d take the bus home from school, and at the bottom of my hill I’d pass 
+					the Apoquague Friends’ Burial Ground,” said Viraj Kumar, founder of YFR. “It wasn’t in great shape, and I thought it’d be pretty cool to help restore it with a group of people.”  
+					He organized a group of student volunteers, made plans to restore the cemetery, sought support from the neighborhood and solicited sponsorship from local businesses and the Town.  
+					The team worked over several weekends to clean-up the site, rebuild the stone wall, erect the fallen head stones, cut down and remove fallen, decayed trees and brush.
+				</p>
+				
+				<h1 class="whitney">History</h1>
+				
+				<p> 
+					The Apoquague Friends’ Meeting was organized in 1771. There are a total of 122 graves.
+				<p></p>
+				
+				<p>
+					For more information, please <a href= "http://www.usgennet.org/usa/ny/county/dutchess/data/cems/apoquaguecem.htm">click here.</a>
+				</p>
+				
+				<p></p>
+				
+				<h1 class="whitney">Overview</h1>
+				
+				<p>Please <a href= "https://www.dropbox.com/sh/8lsp67wzka2kska/AACsy2QKxntJiz8hRbKwiXV3a#/">click here</a> to see a slide show of the site</p>
+				
 			</div>
-			
 		</div>
 		<nav class="navbar navbar-default navbar-fixed-bottom">
 
