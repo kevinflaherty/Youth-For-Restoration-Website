6e11ce1d52e817187d5bc443ecdf798d6d062b16
Kevin Flaherty <kevinjflaherty6@gmail.com>
2015-04-13 20:28:42 -0400

    Project page for Patent Model Museum
    Page for the Patent Model Collection restoration.

---
+++ project1.php
@@ -0,0 +1,138 @@
+<?php
+include "sitecake/sitecake/server/sitecake_entry.php";
+ ?>
+<!DOCTYPE html>
+
+<html lang="en">
+
+	<head>
+		<link rel="icon"
+		type="image/png"
+		href="photos/chrislogo.png">
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- Latest compiled and minified CSS -->
+
+		<!-- Optional theme -->
+		<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+		<link href="bootstrap-3.3.4-dist/css/bootstrap-responsive.css" rel="stylesheet" />
+		<link rel="stylesheet" type="text/css" href="bootstrap-3.3.4-dist/css/bootstrap.min.css">
+		<!-- Latest compiled and minified JavaScript -->
+
+		<title>Youth for Restoration</title>
+
+		<!-- Bootstrap -->
+
+		<style type="text/css">
+			body {
+				padding-top: 70px;
+				padding-bottom: 70px;
+			}
+		</style>
+		<style type="text/css">
+			body {
+
+				background: url(photos/Home.JPG) no-repeat center center fixed;
+				-webkit-background-size: cover;
+				-moz-background-size: cover;
+				-o-background-size: cover;
+				background-size: cover;
+			}
+		</style>
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+	</head>
+	<header>
+
+		<nav class="navbar navbar-default navbar-fixed-top">
+
+			<div class="container-fluid">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+				</div>
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+						<li class="active">
+							<a href="index.php">Home<span class="sr-only">(current)</span></a>
+						</li>
+						<li>
+							<a href="about.php">About Us</a>
+						</li>
+						<li>
+							<a href="projects.php">Projects</a>
+						</li>
+						<li>
+							<a href="news.php">News</a>
+						</li>
+						<li>
+							<a href="#">Contact</a>
+						</li>
+						<li>
+							<a href="#">Donate</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<!-- Collect the nav links, forms, and other content for toggling -->
+
+			</div><!-- /.navbar-collapse -->
+		</nav>
+
+		<!-- /.container-fluid -->
+
+	</header>
+	<body>
+
+		<!-- /.container-fluid -->
+
+		<div class="container" >
+			<div class="jumbotron myBackground sc-content-home">
+				<h1 class="whitney">Youth For Restoration</h1>
+
+				<p>
+					YFR is currently working on creating a database for the largest collection of U.S. patent models by women inventors in existence. These models are part of the Susan M. Eichelberg Glendening Patent Model Collection,
+					and housed in the Riverbank Historic House Museum in Cornwall-on-Hudson, NY.
+				</p>
+				
+				<p>
+					The U.S Patent office was created in 1790. From 1790 until 1880 inventors were required to submit models of their inventions along with their applications. This collection contains over 650 original patent models,
+					including, Thomas Edison's Carbonizer for the Filaments of Light Bulbs (October 18, 1881), the 1879 Bicycle, and perhaps the smallest model ever found of the Safety Pin(1879).
+				</p>
+				
+				<p> 
+					For more information, please <click here>.
+				
+				<p>
+					<a href="about.php" class="btn btn-primary btn-lg" role="button">Learn more &raquo;</a>
+				</p>
+			</div>
+		</div>
+		<nav class="navbar navbar-default navbar-fixed-bottom">
+
+			<div class="container">
+				<a href= "https://www.facebook.com/youthforrestoration"> <img src="photos/facebookfavicon.ico" alt="Facebook" height="25" width="25"></a>
+				<a href= "https://twitter.com/Y_F_R_"><img  src="photos/Twitterfavicon.ico" alt="Twitter"height="25" width="25"></a>
+				<a href= "http://youthforrestoration.blogspot.com/" ><img src="photos/blogspotfavicon.ico" alt="Blog"height="25" width="25"></a>
+			</div>
+		</nav>
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+		<!-- Include all compiled plugins (below), or include individual files as needed -->
+		<script src="js/bootstrap.min.js"></script>
+
+	</body>
+</html>
